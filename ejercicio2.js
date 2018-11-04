0c7a6812e19bdbbfb73704f17d4cff0495321a5c
Hugo <hfernandez@neuquen.gov.ar>
2018-11-04 17:20:38 -0300

    cambios ejercicio2 para hacerlo con post

--- ejercicio2.js
+++ ejercicio2.js
@@ -1,22 +1,23 @@
 const {
     send
 } = require('micro');
+const microBody = require('micro-body')
 
 /**
  * ¡Hola Andinistas!
- * En este ejercicio deberán crear una calculadora básica (suma, resta, multiplicación y división), para ellos deberán enviar en el request un objeto de la forma:
+ * En este ejercicio deberán crear una calculadora básica (suma, resta, multiplicación y división), para ellos deberán enviar en un POST la siguiente información:
  * {operacion: value, operador1: value, operador2: value}
  * Ejemplo: {operacion: suma, operador1: 2, operador2: 3}
- * El microservicio deberá realizar la operación y devolver tipo de operación junto con el resultado obtenido.
+ * El microservicio deberá realizar la operación y devolverá el tipo de operación junto con el resultado obtenido.
  */
 
-// AYUDA: Busque como funciona micro-query
+// AYUDA: Debe usar micro-body para poder realizar el POST
 
 module.exports = async (request, response) => {
     let resultado = 0;
     let operacion = '';
     //TODO
 
-    send(response, 200, `El resultado de la operación ${operacion} es: ${resultado}`);
+    return `El resultado de la operación ${info.op} es igual a ${resultado}`;
 
 }
